4aef736bf7312b5377792c2184873617722309c1
Gevork <g.tarhanyan@gmail.com>
2022-10-03 19:24:37 +0500

    add crypto and create user.dto

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export default class CreateUserDto {
+  public name!: string;
+  public email!: string;
+  public avatarUrl!: string;
+  public password!: string;
+  public isPro!: boolean;
+}
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,5 @@
 import { HousingOfType } from '../types/housing-of-type.enum';
+import crypto from 'crypto';
 
 export const createOffer = (row: string) => {
   const tokens = row.replace('\n', '').split('\t');
@@ -35,3 +36,8 @@ export const createOffer = (row: string) => {
 
 export const getErrorMessage = (error: unknown): string =>
   error instanceof Error ? error.message : '';
+
+export const createSHA256 = (line: string, salt: string): string => {
+  const shaHasher = crypto.createHmac('sha256', salt);
+  return shaHasher.update(line).digest('hex');
+};
