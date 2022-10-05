acef86c766c3d732f7b062e9f6ee392391f636b1
Gevork <64646814+socialcolor@users.noreply.github.com>
2022-10-05 15:58:09 +0500

    Откактил

--- src/modules/offer/offer.entity.ts
+++ src/modules/offer/offer.entity.ts
@@ -1,6 +1,6 @@
-import typegoose, { defaultClasses, getModelForClass } from '@typegoose/typegoose';
-// import { UserEntity } from '../user/user.entity.js';
-// import { HousingOfType } from '../../types/housing-of-type.enum.js';
+import typegoose, { defaultClasses, getModelForClass, Ref } from '@typegoose/typegoose';
+import { UserEntity } from '../user/user.entity.js';
+import { HousingOfType } from '../../types/housing-of-type.enum.js';
 
 const { prop, modelOptions } = typegoose;
 
@@ -17,6 +17,56 @@ export class OfferEntity extends defaultClasses.TimeStamps {
 
   @prop({ trim: true })
   public description!: string;
+
+  @prop()
+  public date!: string;
+
+  @prop()
+  public city!: string;
+
+  @prop()
+  public previewImage!: string;
+
+  @prop()
+  public images!: string;
+
+  @prop()
+  public isPremium!: string;
+
+  @prop()
+  public isFavorite!: string;
+
+  @prop()
+  public rating!: number;
+
+  @prop({
+    type: () => String,
+    enum: HousingOfType,
+  })
+  public type!: HousingOfType;
+
+  @prop()
+  public bedrooms!: number;
+
+  @prop()
+  public maxAdults!: number;
+
+  @prop({ required: true })
+  public price!: number;
+
+  @prop()
+  public goods!: string;
+
+  @prop({
+    ref: UserEntity,
+    required: true,
+    default: {},
+    _id: false,
+  })
+  public host!: Ref<UserEntity>;
+
+  @prop()
+  public comments!: number;
 }
 
 export const OfferModel = getModelForClass(OfferEntity);
